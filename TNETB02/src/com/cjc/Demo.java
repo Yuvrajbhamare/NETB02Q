5deb1c47543cfa056f371392e4df1f550abdc3bb
Lenovo <Lenovo@LAPTOP-LR60D6IR>
2022-08-02 21:01:15 +0530

    sop statement

--- TNETB02/src/com/cjc/Demo.java
+++ TNETB02/src/com/cjc/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello");
+System.out.println("Main method hai");
+System.out.println("hello cjccc");
 	}
 
 }
