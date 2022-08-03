771ac882dcfbfc5bb2cfa21b759ac21362ea79b3
Lenovo <Lenovo@LAPTOP-LR60D6IR>
2022-08-03 10:37:52 +0530

    programe five newww

--- TNETB02/src/com/cjc/Demo.java
+++ TNETB02/src/com/cjc/Demo.java
@@ -11,6 +11,7 @@ System.out.println("program 2 from git to eclipse");
 System.out.println("program 2 from git to eclipse3");
 System.out.println("programe 3 from eclipse to git");
 System.out.println("prgrame 4 from eclipse to git");
+System.out.println("programe 5 from eclipse to git");
 
 
   
