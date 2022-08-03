60c9474af7a6235a255738405567339c9a39d8b3
Lenovo <Lenovo@LAPTOP-LR60D6IR>
2022-08-03 10:07:48 +0530

    method 4

--- TNETB02/src/com/cjc/Demo.java
+++ TNETB02/src/com/cjc/Demo.java
@@ -5,10 +5,12 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		
-  System.out.println("program 2");
+System.out.println("program 2");
 System.out.println("program 2 from git to eclipse");
 System.out.println("program 2 from git to eclipse3");
-System.out.println("programe 3 frome eclipse to git");
+System.out.println("programe 3 from eclipse to git");
+System.out.println("prgrame 4 from eclipse to git");
+
 
   
 
