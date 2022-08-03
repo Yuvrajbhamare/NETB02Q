0d182db5180ec8852845405ec1c714cfd3177e05
Lenovo <Lenovo@LAPTOP-LR60D6IR>
2022-08-03 08:02:05 +0530

    proamme 2,kindly add

--- TNETB02/src/com/cjc/Demo.java
+++ TNETB02/src/com/cjc/Demo.java
@@ -3,12 +3,15 @@ package com.cjc;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello");
-System.out.println("Main method hai");
-System.out.println("hello cjccc");
-System.out.println("git to eclipse");		
 		
+		
+  System.out.println("program 2");
+  
+
 	}
 
 }
+
+
+
+
